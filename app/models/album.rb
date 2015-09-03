f7f3992f5a66239fa041b22b291ddb93c277e722
zachhall <zachhallnc@gmail.com>
2015-09-02 20:59:59 -0400

    add carrierwave and _footer

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,6 @@ class Album < ActiveRecord::Base
 	has_many :songs
 	belongs_to :artist
 
+	mount_uploader :cover, CoverUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902221741) do
+ActiveRecord::Schema.define(version: 20150902235203) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "cover"
   end
 
   create_table "artists", force: :cascade do |t|
